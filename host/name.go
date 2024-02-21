b8e9337f311531c95894b39344ca3fea1c211ccb
Alexander Berresch <alexander.berresch@centerdevice.com>
2024-02-21 11:29:31 +0100

    Hostname should still accept a port like it was before

--- host/name.go
+++ host/name.go
@@ -9,7 +9,7 @@ import (
 	"bosun.org/name"
 )
 
-const hostRegexPattern = `^([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])(\.([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9]))*$`
+const hostRegexPattern = `^([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])(\.([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9]))*(:[0-9]+)?$`
 
 type hostNameFormatConfig struct {
 	useFullName bool
